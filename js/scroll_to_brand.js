fa8f74b04b3123ffa3d3e08123033d774c8a6c3a
borisghidaglia <borisghidaglia@gmail.com>
2017-10-16 14:07:37 +0200

    shop on mobile modified for better responsivity

---
+++ js/scroll_to_brand.js
@@ -0,0 +1,15 @@
+$('#myMenu a').click(function () {
+    if(w < 768){
+        var hash = $(this).attr('href');
+        hash = hash.replace('#','');
+        obj = document.getElementById(hash);
+        header_height = window.getComputedStyle(document.getElementsByTagName('header')[0]).height;
+        header_height = header_height.replace('px','');
+        if (obj) {
+            scrollValue = $(obj).offset().top;
+            $('body,html').animate({
+                'scrollTop' :scrollValue - header_height,
+            },600,'swing');
+        }
+    }
+});
